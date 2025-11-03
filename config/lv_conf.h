cbaa3be311cc72b57008b5db5bcef6e903af35c0
johnhammer9628 <johnhammer9628@gmail.com>
2025-11-03 13:13:25 -0800

    config: set LV_COLOR_DEPTH=32 for RGB888 panel

--- config/lv_conf.h
+++ config/lv_conf.h
@@ -4,4 +4,9 @@
 #define LV_FONT_DEFAULT_MONTSERRAT 1
 #define LV_FONT_MONTSERRAT_20 1
 #define LV_FONT_MONTSERRAT_24 1
+
+// 24-bit RGB panel -> use 32-bit color in LVGL (XRGB8888)
+#define LV_COLOR_DEPTH 32
+
+
 #endif
